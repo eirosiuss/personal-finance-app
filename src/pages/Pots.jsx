5e19ff36005920db5d2384d5cedb9b4c3a76c213
Kestutis Eirosius <eirosiuss@gmail.com>
2025-07-14 17:59:04 +0300

    Add progress bar to Pots component for visual representation of savings, including buttons for adding money and withdrawing.

--- src/pages/Pots.jsx
+++ src/pages/Pots.jsx
@@ -8,8 +8,26 @@ const Pots = () => {
       {pots.map((pot, index) => (
         <article key={index}>
           <h3>{pot.name}</h3>
-          <p>Total Saved<span>${pot.total}.00</span></p>
-          <p>Target of ${pot.target}</p>
+          <p>
+            Total Saved<span>${pot.total}.00</span>
+          </p>
+          <div className="progress-bar">
+            <div
+              className="progress-bar-fill"
+              style={{
+                width: `${(pot.total / pot.target) * 100}%`,
+                backgroundColor: pot.theme,
+              }}
+            ></div>
+          </div>
+          <div className="progress-bar-text">
+            <p>{((pot.total / pot.target) * 100).toFixed(2)}%</p>
+            <p>Target of ${pot.target}</p>
+          </div>
+          <div>
+            <button>+ Add Money</button>
+            <button>Withdraw</button>
+          </div>
         </article>
       ))}
     </>
