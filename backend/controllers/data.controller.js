ef5f5c30f7895e697963111554eead2d871c5071
Kestutis Eirosius <eirosiuss@gmail.com>
2025-08-27 16:44:40 +0300

    Implement add, delete, and edit budget routes and controller methods. Update data routes to include new budget operations.

--- backend/controllers/data.controller.js
+++ backend/controllers/data.controller.js
@@ -3,7 +3,8 @@ import { Data } from "../models/Data.js";
 export const transactions = async (req, res) => {
   try {
     const data = await Data.findOne({ user: req.userId });
-    if (!data) return res.status(404).json({ message: "Transactions not found" });
+    if (!data)
+      return res.status(404).json({ message: "Transactions not found" });
     res.status(200).json(data.transactions);
   } catch (error) {
     console.error("Error fetching transactions:", error);
@@ -30,5 +31,66 @@ export const pots = async (req, res) => {
   } catch (error) {
     console.error("Error fetching pots:", error);
     res.status(500).send("Failed to fetch pots");
+  }
+};
+
+export const addBudget = async (req, res) => {
+  try {
+    const data = await Data.findOne({ user: req.userId });
+    if (!data) data = new Data({ user: req.userId, budgets: [req.body] });
+    data.budgets.push(req.body);
+    await data.save();
+    res.status(200).json(data?.budgets);
+  } catch (error) {
+    console.error("Error adding new budget: ", error);
+    res.status(500).send("Failed to add new budget");
+  }
+};
+
+export const deleteBudget = async (req, res) => {
+  const category = req.params.category;
+  try {
+    const updatedData = await Data.findOneAndUpdate(
+      {},
+      { $pull: { budgets: { category: category } } },
+      { new: true }
+    );
+    if (!updatedData)
+      return res.status(404).json({ message: "Budget not found" });
+    res
+      .status(200)
+      .json({ message: "Budget deleted", budgets: updatedData.budgets });
+  } catch (error) {
+    console.error("Error deleting budget:", error);
+    res.status(500).send("Failed to delete budget");
+  }
+};
+
+export const editBudget = async (req, res) => {
+  const { newTitle, newMaximum, newTheme } = req.body;
+
+  try {
+    const updatedData = await Data.findOneAndUpdate(
+      { "budgets.category": req.params.oldCategory },
+      {
+        $set: {
+          "budgets.$.category": newTitle,
+          "budgets.$.maximum": newMaximum,
+          "budgets.$.theme": newTheme,
+        },
+      },
+      { new: true }
+    );
+
+    if (!updatedData) {
+      return res.status(404).json({ message: "Budget not found" });
+    }
+    res.status(200).json({
+      message: "Budget updated",
+      budgets: updatedData.budgets,
+    });
+  } catch (error) {
+    console.error("Error updating budget:", error);
+    res.status(500).send("Error updating budget");
   }
 };
--- backend/routes/data.route.js
+++ backend/routes/data.route.js
@@ -1,6 +1,13 @@
 import express from "express";
 import { verifyToken } from "../middleware/verifyToken.js";
-import { transactions, budgets, pots } from "../controllers/data.controller.js";
+import {
+  transactions,
+  budgets,
+  pots,
+  addBudget,
+  deleteBudget,
+  editBudget,
+} from "../controllers/data.controller.js";
 
 const router = express.Router();
 
@@ -8,128 +15,8 @@ router.get("/transactions", verifyToken, transactions);
 router.get("/budgets", verifyToken, budgets);
 router.get("/pots", verifyToken, pots);
 
-// router.get("/budgets", async (req, res) => {
-//   try {
-//     const data = await Data.findOne({ user: req.user.id });
-//     if (!data) return res.status(404).json({ message: "No data found" });
-//     res.status(200).json(data.budgets);
-//   } catch (err) {
-//     console.error("Error fetching budgets:", err);
-//     res.status(500).send("Server error");
-//   }
-// });
-
-// router.post("/budgets", async (req, res) => {
-//   try {
-//     let data = await Data.findOne({ user: req.user.id });
-//     if (!data) {
-//       data = new Data({ user: req.user.id, budgets: [req.body] });
-//     } else {
-//       data.budgets.push(req.body);
-//     }
-//     await data.save();
-//     res.status(201).json(data.budgets);
-//   } catch (err) {
-//     res.status(500).send("Server error");
-//   }
-// });
-
-// router.get("/transactions", async (req, res) => {
-//   try {
-//     const data = await Data.findOne();
-//     res.status(200).json(data.transactions);
-//   } catch (error) {
-//     console.error("Error fetching transactions:", error);
-//     res.status(500).send("Failed to fetch transactions");
-//   }
-// });
-
-// router.get("/budgets", async (req, res) => {
-//   try {
-//     const data = await Data.findOne();
-//     res.status(200).json(data.budgets);
-//   } catch (error) {
-//     console.error("Error fetching budgets:", error);
-//     res.status(500).send("Failed to fetch budgets");
-//   }
-// });
-
-// router.post("/budgets", async (req, res) => {
-//   try {
-//     const newBudget = req.body;
-//     let data = await Data.findOne();
-//     if (!data) {
-//       data = new Data({ budgets: [newBudget] });
-//       await data.save();
-//     } else {
-//       data.budgets.push(newBudget);
-//       await data.save();
-//     }
-//     res.status(201).json(data.budgets);
-//   } catch (err) {
-//     res.status(500).send("Server error");
-//   }
-// });
-
-// router.delete("/budgets/:category", async (req, res) => {
-//   const category = req.params.category;
-
-//   try {
-//     const updatedData = await Data.findOneAndUpdate(
-//       {},
-//       { $pull: { budgets: { category: category } } },
-//       { new: true }
-//     );
-
-//     if (!updatedData) {
-//       return res.status(404).json({ message: "Data document not found" });
-//     }
-//     res
-//       .status(200)
-//       .json({ message: "Budget deleted", budgets: updatedData.budgets });
-//   } catch (err) {
-//     console.error("Error deleting budget:", err);
-//     res.status(500).json({ message: "Server error", error: err });
-//   }
-// });
-
-// router.put("/budgets/:oldCategory", async (req, res) => {
-//   const { newTitle, newMaximum, newTheme } = req.body;
-
-//   try {
-//     const updatedData = await Data.findOneAndUpdate(
-//       { "budgets.category": req.params.oldCategory },
-//       {
-//         $set: {
-//           "budgets.$.category": newTitle,
-//           "budgets.$.maximum": newMaximum,
-//           "budgets.$.theme": newTheme,
-//         },
-//       },
-//       { new: true }
-//     );
-
-//     if (!updatedData) {
-//       return res.status(404).json({ message: "Budget not found" });
-//     }
-//     res.status(200).json({
-//       message: "Budget updated",
-//       budgets: updatedData.budgets,
-//     });
-//   } catch (err) {
-//     console.error("Error updating budget:", err);
-//     res.status(500).json({ message: "Server error", error: err });
-//   }
-// });
-
-// router.get("/pots", async (req, res) => {
-//   try {
-//     const data = await Data.findOne();
-//     res.status(200).json(data.pots);
-//   } catch (error) {
-//     console.error("Error fetching pots:", error);
-//     res.status(500).send("Failed to fetch pots");
-//   }
-// });
+router.post("/add-budget", verifyToken, addBudget);
+router.delete("/delete-budget/:category", verifyToken, deleteBudget);
+router.put("/edit-budget/:oldCategory", verifyToken, editBudget);
 
 export default router;
