cfff4f7b0d0899d77d6b96dac082cf9cd955e372
Kestutis Eirosius <eirosiuss@gmail.com>
2025-09-07 09:58:13 +0300

    Add styles to sidebar component for desktop

--- frontend/src/components/shared/Sidebar.jsx
+++ frontend/src/components/shared/Sidebar.jsx
@@ -2,6 +2,8 @@ import { NavLink } from "react-router-dom";
 import { useAuthStore } from "../../store/authStore.js";
 import { Icon } from "@iconify/react";
 import LogoFinance from "../../../src/assets/images/logo-large.svg";
+import LogoFinanceSmall from "../../../src/assets/images/logo-small.svg";
+import { useState } from "react";
 
 const Sidebar = () => {
   const { logout } = useAuthStore();
@@ -9,11 +11,15 @@ const Sidebar = () => {
     logout();
   };
 
+  const [sidebarMinimized, setSidebarMinimized] = useState(false);
+
   const SidebarLink = ({ to, icon, label }) => (
     <NavLink to={to} className="group">
       {({ isActive }) => (
         <div
-          className={`py-2 px-5 md:px-10 md:pt-3 md:flex md:flex-col  md:justify-between md:items-center lg:py-0 lg:px-0 lg:flex-row lg:justify-center lg:items-center ${
+          className={`${
+              sidebarMinimized ? "lg:py-4" : "py-2"
+            } px-5 md:px-10 md:pt-3 md:flex md:flex-col  md:justify-between md:items-center lg:py-0 lg:px-0 lg:flex-row lg:justify-center lg:items-center ${
             isActive
               ? "bg-beige-100 rounded-t-lg border-b-4 border-green group-hover:border-grey-300 lg:rounded-t-none lg:rounded-r-xl lg:border-b-0 lg:border-l-4"
               : "bg-transparent"
@@ -23,14 +29,16 @@ const Sidebar = () => {
             icon={icon}
             width="24"
             height="24"
-            className={`md:pb-1 lg:pb-0  ${
+            className={`md:pb-1 lg:pb-0 ${
               isActive
                 ? "text-green group-hover:text-grey-300"
                 : "text-grey-300 group-hover:text-grey-100"
             }`}
           />
           <p
-            className={`hidden sm:block preset-5-bold lg:text-preset-3 lg:leading-preset-3 lg:font-preset-3 text-center lg:py-4 ${
+            className={`${
+              sidebarMinimized ? "lg:hidden" : ""
+            } hidden sm:block preset-5-bold lg:text-preset-3 lg:leading-preset-3 lg:font-preset-3 text-center lg:py-4 ${
               isActive
                 ? "bg-beige-100 text-grey-900 group-hover:text-grey-300"
                 : "bg-transparent text-grey-300 group-hover:text-grey-100"
@@ -45,12 +53,18 @@ const Sidebar = () => {
 
   return (
     <>
-      <nav className="w-full lg:flex lg:justify-between fixed bottom-0 bg-grey-900 rounded-t-lg lg:flex-col lg:top-0 lg:left-0 lg:h-full lg:w-[300px] lg:rounded-t-none lg:rounded-r-2xl">
+      <nav
+        className={`${
+          sidebarMinimized ? "lg:w-[88px]" : "lg:w-[300px]"
+        } w-full lg:flex lg:justify-between fixed bottom-0 bg-grey-900 rounded-t-lg lg:flex-col lg:top-0 lg:left-0 lg:h-full  lg:rounded-t-none lg:rounded-r-2xl`}
+      >
         <div className="w-full">
           <div className="hidden lg:block py-10 pl-8 pb-16">
-            <img src={LogoFinance} alt="Logo Finance" />
+            <img src={sidebarMinimized ? LogoFinanceSmall : LogoFinance} alt="Logo Finance" />
           </div>
-          <ul className="w-full flex flex-row justify-between items-center pt-2 px-4 md:px-10 lg:flex-col lg:justify-start lg:pt-0 lg:px-0 lg:pr-6">
+          <ul className={`flex flex-row justify-between items-center pt-2 px-4 md:px-10 lg:flex-col lg:justify-start lg:pt-0 lg:px-0 ${
+          sidebarMinimized ? "lg:pr-2" : "lg:pr-6"
+        }`}>
             <li className="md:w-[104px] lg:w-full">
               <SidebarLink to="/home-page" icon="mdi-house" label="Overview" />
             </li>
@@ -76,7 +90,7 @@ const Sidebar = () => {
               <Icon icon="ph:receipt-fill" width="24" height="24" className="hover:text-grey-100" />
             </NavLink>
           </li> */}
-            <li className="cursor-pointer py-2 px-5 h-10 md:w-[104px] md:flex md:flex-col md:items-center md:py-0 md:px-10 lg:w-full group">
+            <li className="cursor-pointer py-2 px-5 h-10 md:w-[104px] md:flex md:flex-col md:items-center md:py-0 md:px-10 lg:w-full group lg:py-4">
               <button
                 className="text-grey-300 group-hover:text-grey-100"
                 onClick={hangleLogout}
@@ -88,14 +102,36 @@ const Sidebar = () => {
                   className="group-hover:text-grey-100"
                 />
               </button>
-              <p className="hidden mb-1 md:block text-grey-300 preset-5-bold lg:text-preset-3 lg:leading-preset-3 lg:font-preset-3 group-hover:text-grey-100">
+              <p
+                className={`${
+                  sidebarMinimized ? "lg:hidden" : ""
+                } hidden mb-1 md:block text-grey-300 preset-5-bold lg:text-preset-3 lg:leading-preset-3 lg:font-preset-3 group-hover:text-grey-100`}
+              >
                 Logout
               </p>
             </li>
           </ul>
         </div>
-        <div className="hidden lg:block preset-3 pb-20">
-          <p>Minimize Menu</p>
+
+        <div
+          onClick={() => setSidebarMinimized(!sidebarMinimized)}
+          className="hidden preset-3 mb-16 py-5 lg:flex gap-5 items-center text-grey-300 group hover:grey-100 cursor-pointer"
+        >
+          <Icon
+            icon={`${
+              sidebarMinimized
+                ? "ph:arrow-fat-lines-right-fill"
+                : "ph:arrow-fat-lines-left-fill"
+            }`}
+            width="24"
+            height="24"
+            className="ml-8 group-hover:text-grey-100"
+          />
+          <button className={`preset-3 cursor-pointer group-hover:text-grey-100 ${
+                  sidebarMinimized ? "lg:hidden" : ""
+                }`}>
+            Minimize Menu
+          </button>
         </div>
       </nav>
     </>
