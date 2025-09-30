6f997b99392993b4e3faca25cf164f89555ef06b
Kestutis Eirosius <eirosiuss@gmail.com>
2025-09-30 13:13:01 +0300

    Add sendPasswordResetEmail components

--- backend/mailgun/emails.js
+++ backend/mailgun/emails.js
@@ -35,9 +35,28 @@ export const sendWelcomeEmail = async (email, name) => {
       from: process.env.MAILGUN_FROM,
       to: email,
       subject: "Welcome to Personal Finance App",
-      template: "Welcome Email",
+      template: "welcome email",
       "h:X-Mailgun-Variables": JSON.stringify({
         company_info_name: "Personal Finance App",
+        name: name,
+      }),
+    });
+    console.log("Email sent successfully:", data);
+  } catch (error) {
+    console.error("Error sending verification email", error);
+    throw new Error(`Error sending verification email: ${error}`);
+  }
+};
+
+export const sendPasswordResetEmail = async (name, email, resetURL) => {
+  try {
+    const data = await mg.messages.create(process.env.MAILGUN_DOMAIN, {
+      from: process.env.MAILGUN_FROM,
+      to: email,
+      subject: "Forgot Password",
+      template: "forgot password",
+      "h:X-Mailgun-Variables": JSON.stringify({
+        resetURL: resetURL,
         name: name,
       }),
     });
