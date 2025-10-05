4e7386d9ef52194df358508c817d971a5dbad04c
Kestutis Eirosius <eirosiuss@gmail.com>
2025-10-05 19:52:19 +0300

    fix height for dropdown menu

--- frontend/src/components/pages/Transactions.jsx
+++ frontend/src/components/pages/Transactions.jsx
@@ -486,7 +486,7 @@ export default function Transactions() {
                       name="sort"
                       value={selectedSort}
                       onChange={handleSort}
-                      className="w-30 px-5 pt-3 absolute left-1/2 -translate-x-1/2 top-28 z-10 bg-white rounded-lg text-grey-900 preset-4"
+                      className="w-30 h-64 px-5 pt-3 absolute left-1/2 -translate-x-1/2 top-28 z-10 bg-white rounded-lg text-grey-900 preset-4"
                     >
                       <option
                         className="border-b border-b-grey-100 pb-3 mb-3"
@@ -563,7 +563,7 @@ export default function Transactions() {
                       value={selectedCategory}
                       size={8}
                       onChange={handleCategoryChange}
-                      className="w-fit px-5 pt-3 absolute left-1/2 -translate-x-1/2 top-28 z-10 bg-white rounded-lg text-grey-900 preset-4"
+                      className="w-fit h-64 px-5 pt-3 absolute left-1/2 -translate-x-1/2 top-28 z-10 bg-white rounded-lg text-grey-900 preset-4"
                     >
                       <option
                         className="border-b border-b-grey-100 pb-3 mb-3"
